dabd7631b258145d90f937d20fd2f2b23adbbebd
Juny <junhee.kyobolife@gmail.com>
2023-03-23 21:03:51 +0900

    서버시간 서울 시간대로 적용

--- src/main/java/com/kyobo/platform/donots/MicroserviceDonotsAdminApplication.java
+++ src/main/java/com/kyobo/platform/donots/MicroserviceDonotsAdminApplication.java
@@ -3,10 +3,13 @@ package com.kyobo.platform.donots;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 public class MicroserviceDonotsAdminApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(MicroserviceDonotsAdminApplication.class, args);
     }
 }
