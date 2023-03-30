8f98338c621a5adaf4c771c2a77ab9b407c86c91
Juny <junhee.kyobolife@gmail.com>
2023-03-30 15:51:01 +0900

    로깅 추가

--- src/main/java/com/kyobo/platform/donots/common/filter/CorsFilter.java
+++ src/main/java/com/kyobo/platform/donots/common/filter/CorsFilter.java
@@ -1,5 +1,6 @@
 package com.kyobo.platform.donots.common.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 
 /**
@@ -16,11 +18,36 @@ import java.io.IOException;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
 public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
+
+        log.info("RemoteAddr: "+ request.getRemoteAddr());
+        log.info("RemoteHost: "+ request.getRemoteHost());
+        log.info("RemotePort: "+ request.getRemotePort());
+        log.info("RemoteUser: "+ request.getRemoteUser());
+        log.info("RequestURI: "+ request.getRequestURI());
+        log.info("RequestURL: "+ request.getRequestURL());
+        log.info("ServerName: "+ request.getServerName());
+        log.info("ServerPort: "+ request.getServerPort());
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+        log.info("HttpServletRequest.parameters are as follows if any:");
+        while (parameterNames.hasMoreElements()) {
+            String currentElem = parameterNames.nextElement();
+            log.info(currentElem + ": " + request.getParameter(currentElem.toString()));
+        }
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        log.info("HttpServletRequest.headers are as follows if any:");
+        while (headerNames.hasMoreElements()) {
+            String currentElem = headerNames.nextElement();
+            log.info(currentElem + ": " + request.getHeader(currentElem.toString()));
+        }
+
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
