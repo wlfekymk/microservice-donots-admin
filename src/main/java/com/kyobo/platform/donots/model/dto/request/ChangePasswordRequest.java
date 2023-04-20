354ca2d2caf3987953ee3345d1ed8c6cacc69f76
이로얌 <wlfekymk@gmail.com>
2023-04-20 15:20:00 +0900

    [SM-642] 로그인 & 비밀번호 변경 시 Validation Check 해제

--- src/main/java/com/kyobo/platform/donots/model/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/kyobo/platform/donots/model/dto/request/ChangePasswordRequest.java
@@ -11,7 +11,7 @@ public class ChangePasswordRequest {
     public ChangePasswordRequest(){}
 
     @NotBlank
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}", message = "비밀번호는 8~15자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    //@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}", message = "비밀번호는 8~15자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     @Schema(description = "현재 비밀번호")
     private String password;
 
--- src/main/java/com/kyobo/platform/donots/model/dto/request/SignInRequest.java
+++ src/main/java/com/kyobo/platform/donots/model/dto/request/SignInRequest.java
@@ -12,12 +12,12 @@ public class SignInRequest {
     public SignInRequest(){}
 
     @NotBlank
-    @Pattern(regexp = "^[a-zA-Z0-9]{5,20}$", message = "아이디는 영문자(대,소문자) 숫자로 5~20자리이어야 합니다.")
+    //@Pattern(regexp = "^[a-zA-Z0-9]{5,20}$", message = "아이디는 영문자(대,소문자) 숫자로 5~20자리이어야 합니다.")
     @Schema(description = "아이디")
     private String adminId;
 
     @NotBlank
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}", message = "비밀번호는 8~15자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    //@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}", message = "비밀번호는 8~15자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     @Schema(description = "현재 비밀번호")
     private String password;
 }
