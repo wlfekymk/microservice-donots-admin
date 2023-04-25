84e62e63124628db7d56a1c94bf7fb3bf373725a
이로얌 <wlfekymk@gmail.com>
2023-04-25 13:54:00 +0900

    [SR-583]  비밀번호 N회 틀릴경우 차단로직, ADMIN 공통 bug fix

--- src/main/java/com/kyobo/platform/donots/model/dto/request/PasswordUnlockRequest.java
+++ src/main/java/com/kyobo/platform/donots/model/dto/request/PasswordUnlockRequest.java
@@ -17,7 +17,6 @@ public class PasswordUnlockRequest {
     private String adminId;
 
     @NotBlank
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}", message = "비밀번호는 8~15자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     @Schema(description = "작업 관리자 비밀번호")
     private String regeditAdminPassword;
 
